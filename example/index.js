3ec0370d96a666eaa44b4fc8b57e2d198d0cfcf2
David Emory <demory@conveyal.com>
2015-01-30 15:27:09 -0500

    Add example

---
+++ example/index.js
@@ -0,0 +1,39 @@
+var Transitive = require('transitive');
+
+// set some Leaflet map configuration properties
+var config = {
+    tileUrl : 'http://{s}.tiles.mapbox.com/v3/conveyal.ie3o67m0/{z}/{x}/{y}.png',
+    initLatLng : new L.LatLng(38.903324,-77.04448), // DC
+    initZoom : 13
+};
+
+// set up the leaflet map, disabling inertia and zoom animation
+var map = L.map('map', {
+  inertia: false,
+  zoomAnimation: false
+});
+
+// add the base TileLayer
+map.addLayer(new L.TileLayer(config.tileUrl));
+
+// create the Transitive instance
+var transitive = new Transitive({
+  data: DATA,
+  useDynamicRendering: true,
+  draggableTypes: ['PLACE']
+});
+
+// create and add the Transitive layer to the map
+map.addLayer(new L.TransitiveLayer(transitive));
+
+// set the initial map view
+map.setView(config.initLatLng, config.initZoom);
+
+// set up the demo clear/load controls
+document.getElementById("clear").onclick = function(event) {
+  transitive.clearData();
+};
+
+document.getElementById("load").onclick = function(event) {
+  transitive.updateData(DATA);
+};
